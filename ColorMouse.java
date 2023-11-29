474a7f9fd4a855f85651323684531a644e399417
calleshouse93 <149311867+calleshouse93@users.noreply.github.com>
2023-11-28 21:09:45 -0500

    Add files via upload

---
+++ ColorMouse.java
@@ -0,0 +1,37 @@
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ColorMouse extends Application {
+
+	public void start(Stage primaryStage) {
+		StackPane pane = new StackPane();
+
+		Circle circle = new Circle(50);
+		circle.setFill(Color.WHITE);
+		circle.setStroke(Color.BLACK);
+		pane.getChildren().add(circle);
+		
+		pane.setOnMousePressed(e -> {
+			circle.setFill(Color.BLACK);
+			
+		});
+
+		pane.setOnMouseReleased(e -> {
+			circle.setFill(Color.WHITE);
+			circle.setStroke(Color.BLACK);
+			
+		});
+
+
+		Scene scene = new Scene(pane, 120, 120);
+		primaryStage.setTitle("ColorMouse");
+		primaryStage.setScene(scene); 
+		primaryStage.show(); 
+	}
+}
+
